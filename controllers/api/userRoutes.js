583d2848f758e5624be7852f32e5a19d23ff200f
mameami26 <mameami26@gmail.com>
2024-10-23 20:49:56 -0400

    finish up my welcome page design

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,42 +1,34 @@
 const router = require("express").Router();
 const { User } = require("../../models");
 
+// Register route
 router.post("/register", async (req, res) => {
   try {
     const userData = await User.create(req.body);
-    console.log(userData);
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
       res.status(200).json(userData);
     });
   } catch (err) {
-    console.log(err);
-    res.status(400).json(err);
+    console.error(err);
+    res.status(400).json({ message: 'Failed to register', error: err });
   }
 });
 
+// Login route
 router.post("/login", async (req, res) => {
-  console.log(req.body)
   try {
-    const userData = await User.findOne({
-      where: { username: req.body.username },
-    });
-    console.log(userData);
+    const userData = await User.findOne({ where: { username: req.body.username } });
+
     if (!userData) {
-      res
-        .status(400)
-        .json({ message: "Incorrect username or password, please try again" });
-      return;
+      return res.status(400).json({ message: "Incorrect username or password" });
     }
 
     const validPassword = await userData.checkPassword(req.body.password);
 
     if (!validPassword) {
-      res
-        .status(400)
-        .json({ message: "Incorrect username or password, please try again" });
-      return;
+      return res.status(400).json({ message: "Incorrect username or password" });
     }
 
     req.session.save(() => {
@@ -45,10 +37,12 @@ router.post("/login", async (req, res) => {
       res.json({ user: userData, message: "You are now logged in!" });
     });
   } catch (err) {
-    res.status(400).json(err);
+    console.error(err);
+    res.status(400).json({ message: 'Failed to log in', error: err });
   }
 });
 
+// Logout route
 router.post("/logout", (req, res) => {
   if (req.session.logged_in) {
     req.session.destroy(() => {
--- models/User.js
+++ models/User.js
@@ -19,7 +19,14 @@ User.init(
     username: {
       type: DataTypes.STRING,
       allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
       unique: true,
+      validate: {
+        isEmail: true,
+      },
     },
     password: {
       type: DataTypes.STRING,
@@ -32,12 +39,16 @@ User.init(
         newUserData.password = await bcrypt.hash(newUserData.password, 10);
         return newUserData;
       },
+      beforeUpdate: async (updatedUserData) => {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+      },
     },
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'user_db',
+    modelName: 'user',
   }
 );
 
